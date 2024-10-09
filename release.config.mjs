a27efed2c1a390d9fd65ee64a9265e7323f5ab20
Radosław Grochowski <rg@fard.pl>
2024-10-09 20:51:38 +0200

    feat: update packages and purge config
    BREAKING CHANGE: This relies on eslint 9 from now on

--- release.config.mjs
+++ release.config.mjs
@@ -3,11 +3,11 @@
  */
 export default {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    '@semantic-release/git',
-    '@semantic-release/changelog',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/git",
+    "@semantic-release/changelog",
   ],
 };
