616af8b9616a52149a37740bb1a27abcdf846e23
Radosław Grochowski <rg@fard.pl>
2025-04-28 17:57:05 +0200

    chore(semantic-release): shorten release message

--- release.config.mjs
+++ release.config.mjs
@@ -5,9 +5,12 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git",
-    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      { message: "chore(release): ${nextRelease.version} [skip ci]" },
+    ],
   ],
 };
