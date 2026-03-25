d1486c3c5213aa8f28fea050c4b6d4ba6d4c9158
Radosław Grochowski <rg@fard.pl>
2026-03-25 18:45:51 +0100

    chore(flake): add explicit inputs block for Renovate compatibility

--- flake.nix
+++ flake.nix
@@ -1,5 +1,11 @@
 {
   description = "radoslawgrochowski/eslint-config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs =
     { self, nixpkgs, flake-utils, }:
     flake-utils.lib.eachDefaultSystem
